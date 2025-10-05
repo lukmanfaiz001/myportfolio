6cd7e88390609fb4d84fa1d18d3d9b42cc8c771d
lukmanfaiz001 <lukmanfaiz001@gmail.com>
2025-10-05 22:45:25 +0530

    Add files via upload

---
+++ proj.js
@@ -0,0 +1,4 @@
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  document.getElementById("formMessage").textContent = "Thank you for your message!";
+});
